6940a9c7f7c4b90b398d83decba3ce63b129d5ac
TJ-Projects3 <tjprojects3@gmail.com>
2024-09-02 14:41:15 -0400

    Added homepage button and homepage link to  image, along with small SEO boost with alt descriiption

---
+++ app/learn-more/page.js
@@ -0,0 +1,56 @@
+'use client';
+import React from 'react';
+import { Container, Typography, Button, Box, Grid, Paper } from '@mui/material';
+import { styled } from '@mui/system';
+import { Height, Widgets } from '@mui/icons-material';
+
+
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(3),
+  backgroundColor: '#f5f5f5', // Light grey background
+  color: '#333', // Dark text color
+}));
+
+const LearnMore = () => {
+  return (
+    <Box
+    sx={{
+      backgroundImage: 'url("")',
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      width: "100vw",
+      height: "100vh",
+      justifyContent: "center",
+      flexDirection: "column",
+      alignItems: "center",
+      overflow: "scroll",
+    }}
+    >
+    <Container maxWidth="lg" sx={{ marginTop: 4}}>
+      <Typography variant="h2" align="center" gutterBottom>
+        Contact or connect with our founders
+      </Typography>
+      <Typography variant="body1" align="center" color="textSecondary" paragraph>
+        RMP AI is a platform that helps students make informed and smarter decisions for their classes and professors. 
+        Explore detailed reviews, ratings, and comments from students just like you.
+      </Typography>
+      <Box sx={{width:1200, height:500, backgroundColor: '#8ABD91', borderRadius:80, display: 'flex'}} ml={'auto'} mr={'auto'}>
+        <img src=""></img>
+      </Box>
+      <Box mt={4} textAlign="center">
+        <Typography variant="h6" gutterBottom>
+          Have Questions?
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          Visit our FAQ section to find answers to common questions or contact our support team for more information.
+        </Typography>
+        <Button variant="outlined" color="primary" sx={{ marginTop: 2 }}>
+          Learn More
+        </Button>
+      </Box>
+    </Container>
+    </Box>
+  );
+};
+
+export default LearnMore;
--- component/Navbar.jsx
+++ component/Navbar.jsx
@@ -1,21 +1,24 @@
 import { AppBar, Button, Toolbar, Typography } from "@mui/material";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
 
 export default function CustomAppBar() {
     return (
         <AppBar position="static" className="nav-bar" sx={{ backgroundColor: '#b3bfe0' }} height='20vh'>
             <Toolbar className="tool-bar">
                 <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <img src="\RMP.png" width="50px"></img>
+                    <Link href={'/homepage'}><img src="\RMP.png" alt="Student Professor Searcher RMP AI Logo"width="50px"></img></Link>
                     <Typography className="logo-title" component="a" href="/" variant="h6" sx={{  textDecoration: 'none', color: 'black', paddingLeft: '10px'}}>
                         RMP
                     </Typography>
                 </div>
                 <div>
+                <Link href={'/homepage'}><Button color="inherit" className="nav-item" sx={{ color:'black'}}>Home</Button></Link>
                     <Button color="inherit" href="#About" className="nav-item" sx={{ color:'black'}}>About</Button>
                     <Button color="inherit" href="#FAQ" className="nav-item" sx={{ color:'black'}}>FAQ</Button>
                     <Button color="inherit" href="/resources" className="nav-item" sx={{ color:'black'}}>Resource</Button>
-                    <Button color="inherit" href="/contact" className="nav-item" sx={{ color:'black'}}>Contact</Button>    
+                    <Link href={'/learn-more'}><Button color="inherit" href="/contact" className="nav-item" sx={{ color:'black'}}>Contact</Button></Link>    
                 </div>
                 <div>
                     <SignedOut>
